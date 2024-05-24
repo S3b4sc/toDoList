68bc67fc47bab3dcae310652affb49a4eeeaa362
Sebastian Carrillo Mejia <sebastian.carrillo1@udea.edu.co>
2024-05-23 19:25:36 -0500

    Created firestore_service.py to extract information from the data base fro mthe cloud, Now, from main we can get the todo list when a user logs in, also updatee requirements.txt to use gcloud, macros is to use a for that prints the todos.

---
+++ app/firestore_service.py
@@ -0,0 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials  #Used to communicate
+from firebase_admin import firestore
+
+
+credential = credentials.ApplicationDefault()
+firebase_admin.initialize_app(credential)
+
+#Now create instance of the class client 
+db = firestore.client()
+
+#To get the users list
+def get_users():
+    return db.collection('users').get()
+
+# the \ is to be able to use enter
+def get_todos(user_id):
+    return db.collection('users')\
+        .document(user_id)\
+        .collection('todos').get()
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import unittest
 from app import create_app
 from app.forms import LoginForm
 
+from app.firestore_service import get_users, get_todos
+
 app = create_app()
  
  #------------------To run tests
@@ -26,12 +28,22 @@ def index():
 @app.route('/hello', methods= ['GET'])  #For the  form to be allowed.
 def hello():
     
+    
     user_ip = session.get('user_ip')
     username = session.get('username')
     
     context = {
         'user_ip': user_ip,
+        'todos': get_todos(user_id=username), #get the todos from  the logged user
         'username': username
+    
     }
     
+    users = get_users()
+    
+    for user in users:
+        print(user.id)
+        print(user.to_dict()['password'])
+    
     return render_template('hello.html', **context)
+    
